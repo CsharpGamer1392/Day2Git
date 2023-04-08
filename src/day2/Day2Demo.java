ed78c96ef3045b3471a5e43bfd80fde3ebb39568
GE66_Rider <GE66_Rider@DESKTOP-M5L0776>
2023-04-07 20:49:38 -0700

    team 4 Work

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -6,4 +6,14 @@ public class Day2Demo {
 		System.out.println("Pushing code...");
 		System.out.println("Pushing 2nd time..");
 	}
+	
+	
+	
+	
+	
+	
+	public void team4Demo() {
+		
+		System.out.println("Team 4 demo");
+	}
 }
