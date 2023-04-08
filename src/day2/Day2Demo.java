78ae6bd138ad32ca6ce28c6d2d68691a2616a3c4
GE66_Rider <GE66_Rider@DESKTOP-M5L0776>
2023-04-07 20:26:15 -0700

    Implemented GetInfo()

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -1,9 +1,8 @@
 package day2;
 
 public class Day2Demo {
-	public static void main(String[] args) {
-		
-		System.out.println("Day 2 to git");
+	
+	public void getInfo() {
+		System.out.println("Pushing code...");
 	}
-
 }
